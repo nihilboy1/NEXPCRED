3f73df493474eed75e551331b9c07ce1f5d20afa
nihilboy1 <samuelseve1@gmail.com>
2023-08-05 10:20:44 -0300

    att

--- FGTS_CONSULTAS/src/googlefunctions.ts
+++ FGTS_CONSULTAS/src/googlefunctions.ts
@@ -1,56 +1,63 @@
-import { google } from "googleapis";
+const fs = require("fs").promises;
+const path = require("path");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
+const SPREADSHEETID = "";
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "nexpcred-credentials.json");
 
-export async function getDataFromGoogleSheets(
-  spreadsheetId: string,
-  range: string
-) {
-  const { googleSheets, auth } = await googleSheetsAuth();
-  const rows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range,
-    valueRenderOption: "UNFORMATTED_VALUE",
-    dateTimeRenderOption: "FORMATTED_STRING",
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+async function saveCredentials(client: any) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
   });
-
-  return rows.data.values;
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+async function listMajors(auth: any) {
+  const sheets = google.sheets({ version: "v4", auth });
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
+    range: "Class Data!A2:E",
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log("No data found.");
+    return;
+  }
+  console.log("Name, Major:");
+  rows.forEach((row: any) => {
+    // Print columns A and E, which correspond to indices 0 and 4.
+    console.log(`${row[0]}, ${row[4]}`);
+  });
 }
 
-export async function updateDataToGoogleSheets(
-  spreadsheetId: string,
-  range: string,
-  values: string[][]
-) {
-  const { googleSheets, auth } = await googleSheetsAuth();
-  const request = {
-    auth,
-    spreadsheetId,
-    range,
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values,
-    },
-  };
-
-  await googleSheets.spreadsheets.values.update(request);
-}
-
-export async function googleSheetsAuth() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const client = await auth.getClient();
-
-  const googleSheets = google.sheets({
-    version: "v4",
-    auth: client as any,
-  });
-
-  return {
-    auth,
-    client,
-    googleSheets,
-  };
-}
+authorize().then(listMajors).catch(console.error);
